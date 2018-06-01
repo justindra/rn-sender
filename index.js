e9b2e341e5d396c77fd91a82ea10d5391fc94bb8
Justin Rahardjo <justindra91@gmail.com>
2018-06-02 00:08:35 +1000

    feat(all): Updated script to send to slack

--- index.js
+++ index.js
@@ -3,30 +3,88 @@
 const program = require('commander');
 const config = require('./package.json');
 const GitHub = require('github-api');
-
-const token = '';
+const rp = require('request-promise');
+const md2json = require('md-2-json');
 
 program
   .version(config.version)
   .description(config.description);
 
 program
-  .command('get-latest <user> <repo>')
+  .command('post-latest <user> <repo> [pretext]')
   .alias('l')
-  .description('Get the latest release note for a particular repo')
+  .description('Post the latest release note for a particular repo to slack')
   .option('-t, --token <token>', 'Your Github Personal Access Token')
-  .action((user, repo, cmd) => {
-    console.log(user, repo, cmd.token);
+  .option('-s, --slack-url <url>', 'The slack webhook url')
+  .action((user, repo, pretext, cmd) => {
     const gh = new GitHub({
-      token: cmd.token || token
+      token: cmd.token
     });
 
     const ghRepo = gh.getRepo(user, repo);
     ghRepo.listReleases()
       .then(result => {
-        const latestRelease = result.data[0];
+        const latestRelease = getLatestRelease(result.data);
         const body = latestRelease.body;
+        const username = latestRelease.author.login;
+        const useravatar = latestRelease.author.avatar_url;
+        const title = latestRelease.name;
+        const url = latestRelease.html_url;
+        const fields = generateFields(body);
+        const options = {
+          method: 'POST',
+          uri: cmd.slackUrl,
+          body: {
+            "attachments": [
+              {
+                "fallback": title,
+                "pretext": pretext,
+                "color": "#36a64f",
+                "title": title,
+                "title_link": url,
+                "fields": fields,
+                "footer": username,
+                "footer_icon": useravatar,
+                "mrkdwn_in": ["text"]
+              }
+            ]
+          },
+          json: true // Automatically stringifies the body to JSON
+      };
+      
+      rp(options)
+          .then(function (parsedBody) {
+              console.log(parsedBody);
+          })
+          .catch(function (err) {
+              console.error(err);
+          });
       });
   });
 
 program.parse(process.argv);
+
+function generateFields(raw) {
+  const json = md2json.parse(raw);
+  const fields = [];
+  const types = ['Features', 'Fixes', 'Chores'];
+  for (const type of types) {
+    if (json[type]) {
+      fields.push({
+        title: type,
+        value: json[type].raw,
+        short: false
+      });
+    }
+  }
+  return fields;
+}
+
+function getLatestRelease(releases) {
+  for (const x of releases) {
+    if (!x.draft) {
+      return x;
+    }
+  }
+  return releases[0];
+}
